3f8fc7d1b3e36779c1703db859f40d6b378d7ed9
ecoriya <riyakatikala@gmail.com>
2025-05-30 15:54:33 +0530

    Connected backend to Railway MySQL

--- db.js
+++ db.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-db.connect(err => {
+connection.connect((err) => {
   if (err) throw err;
-  console.log('✅ MySQL connected');
+  console.log('✅ MySQL connected to Railway DB');
 });
 
-module.exports = db;
+module.exports = connection;
